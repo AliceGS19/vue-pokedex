2c416d86b85c57ef512243ea73e358d4b23168df
Alice Gonçalves <galice9992@gmail.com>
2022-01-03 13:43:56 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-var-requires */
-//Referencia: https://dev.to/anjolaogunmefun/deploy-vue-js-projects-to-heroku-1hb5;
+
 
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static')
 const app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
-const PORT = process.env.PORT;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`listening on port ${PORT}, ${path.join(__dirname, "dist")} `));
