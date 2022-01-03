673b282d5d194f89c16495ec944a1e3ae0f6d32b
Alice Gonçalves <galice9992@gmail.com>
2022-01-03 13:52:49 -0300

    URL e PORT passados por variáveis de ambiente

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static')
 const app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`listening on port ${PORT}, ${path.join(__dirname, "dist")} `));
